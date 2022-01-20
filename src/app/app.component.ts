4e09e3ef003c8e0ce966b7c1cf170b49cba0b2ab
sohaibsalman <sohaibsalman10@gmail.com>
2022-01-20 14:21:59 +0500

    Initial section project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { Server } from './server.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+    serverElements: Server[] = [];
+    newServerName = '';
+    newServerContent = '';
+
+    onAddServer() {
+        this.serverElements.push({
+            type: 'server',
+            name: this.newServerName,
+            content: this.newServerContent,
+        });
+    }
+
+    onAddBlueprint() {
+        this.serverElements.push({
+            type: 'blueprint',
+            name: this.newServerName,
+            content: this.newServerContent,
+        });
+    }
 }
