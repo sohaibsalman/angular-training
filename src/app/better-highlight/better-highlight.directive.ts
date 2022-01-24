76d60f9ee5e892c12f707da239bd23af1dbc26c0
sohaibsalman <sohaibsalman10@gmail.com>
2022-01-24 11:22:07 +0500

    HostBinding and HostListener examples

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 /**
  * Another example of custom direcive, but this time using renderer2 from angular
@@ -9,14 +16,32 @@ import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
+  // @HostBinding decorator allows to bind to the html property for the element the directive is used on
+  @HostBinding('style.backgroundColor') backroundColor: string = 'transparent';
+
   // Inject the ElementRef and Renderer2 as properties in this class
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit() {
-    this.renderer.setStyle(
-      this.elRef.nativeElement, // element to style
-      'background-color', // css property name
-      'blue' // css property value
-    );
+  ngOnInit() {}
+
+  // HostListener decorator allows to listen to the HTML events on the element
+  @HostListener('mouseenter') onMouseEnter(eventData: Event) {
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement, // element to style
+    //   'background-color', // css property name
+    //   'blue' // css property value
+    // );
+
+    this.backroundColor = 'blue';
+  }
+
+  @HostListener('mouseleave') onMouseLeave(eventData: Event) {
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement, // element to style
+    //   'background-color', // css property name
+    //   'transparent' // css property value
+    // );
+
+    this.backroundColor = 'transparent';
   }
 }
