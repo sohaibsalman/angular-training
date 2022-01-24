fd140869e46d3e7dd0566f783d5fe197ef3a58aa
sohaibsalman <sohaibsalman10@gmail.com>
2022-01-24 10:41:10 +0500

    Using ngFor and ngIf together

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  evenNumbers = [2, 4];
+  oddNumbers = [1, 3, 5];
+  onlyOdd = false;
 }
