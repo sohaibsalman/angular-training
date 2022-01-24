5848b6e371c0b7b189e79914d0cb61fb0ba58f4c
sohaibsalman <sohaibsalman10@gmail.com>
2022-01-24 17:54:27 +0500

    Create data service

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,26 +1,26 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { LoggingService } from '../logging.service';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService], // The provider tells angular of how to create the object
+  providers: [LoggingService, AccountService], // The provider tells angular of how to create the object
 })
 export class AccountComponent {
   @Input() account!: { name: string; status: string };
   @Input() id!: number;
-  @Output() statusChanged = new EventEmitter<{
-    id: number;
-    newStatus: string;
-  }>();
 
   // Inject the logging service into the component
-  constructor(private loggingService: LoggingService) {}
+  constructor(
+    private loggingService: LoggingService,
+    private accountsService: AccountService
+  ) {}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({ id: this.id, newStatus: status });
+    this.accountsService.updateStatus(this.id, status);
     // use the injected service
     this.loggingService.logStatusChange(status);
   }
