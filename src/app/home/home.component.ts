394e4a703ea91115ea42d3a33c914847a98ecc72
sohaibsalman <sohaibsalman10@gmail.com>
2022-01-27 12:11:41 +0500

    Navigating Programmatically

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  // Inject the router object
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  handleLoadServers() {
+    // Use navigate function to navigate to the component programmatically
+    this.router.navigate(['/servers']);
   }
-
 }
