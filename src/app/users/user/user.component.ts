c7a29c963df41ddc4891c03d7cefa9fc99b99cde
sohaibsalman <sohaibsalman10@gmail.com>
2022-01-27 14:22:36 +0500

    Fetching Route Parameters Reactively

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -16,6 +16,22 @@ export class UserComponent implements OnInit {
     this.user = {
       id: this.route.snapshot.params['id'], // get the id from route parameter
       name: this.route.snapshot.params['name'], // get the name from route parameter
-    };;
+    };
+
+    /**
+     * Example for reacting to parameters change in the component.
+     *
+     * If a component is re-navigated again from the component itself, angular will not
+     * call the ngOnit to initialize th component again. So, in order to update the data of
+     * the component rendered by re-navigating the component again, we need to listen
+     * to the observer.
+     *
+     * Note - if you are 100% sure that the component will not reloaded from within the component,
+     * then we don't need to subscribe. In all other cases, make sure to implement this in ngOnInit
+     * */
+    this.route.params.subscribe((params: Params) => {
+      this.user.id = params['id'];
+      this.user.name = params['name'];
+    });
   }
 }
